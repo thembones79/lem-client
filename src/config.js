f29f083215251d237dc85ef14a2009e12e07df6b
thembones79 <michal.szumnarski@gmail.com>
2020-07-29 08:58:21 +0200

    added  logic to config.js to separate dev and prod

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
-export const ROOT_URL = "https://riverdi-lem.herokuapp.com";
+// export const ROOT_URL = "https://riverdi-lem.herokuapp.com";
 
 // export const ROOT_URL = "http://localhost:3090";
+
+export const ROOT_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://riverdi-lem.herokuapp.com"
+    : "http://localhost:3090";
