544ca6cbbb8575baadf4a078cbe0cc438a98ee54
thembones79 <michal.szumnarski@gmail.com>
2020-08-15 12:31:50 +0200

    fixed timezone issue and time formatting

--- src/components/Scanner/ScanContent.js
+++ src/components/Scanner/ScanContent.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
 
 class ScanContent extends Component {
+  concatenateZeroIfLessThanTen(number) {
+    return number < 10 ? "0" + number : number;
+  }
+
+  renderTime(timeUTC) {
+    if (!timeUTC) {
+      return;
+    }
+    const localTime = new Date(timeUTC);
+    const year = this.concatenateZeroIfLessThanTen(localTime.getFullYear());
+    const month = this.concatenateZeroIfLessThanTen(localTime.getMonth() + 1);
+    const day = this.concatenateZeroIfLessThanTen(localTime.getDate());
+    const hours = this.concatenateZeroIfLessThanTen(localTime.getHours());
+    const minutes = this.concatenateZeroIfLessThanTen(localTime.getMinutes());
+    const seconds = this.concatenateZeroIfLessThanTen(localTime.getSeconds());
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
   render() {
     return (
       <div>
-        <div>{this.props.timeStamp}</div>
+        <div>{this.renderTime(this.props.timeStamp)}</div>
         <div>
           {this.props.scanContent}
           <span>{this.props.errorCode}</span>
